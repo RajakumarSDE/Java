733be9e1c4e142acba1d0030ccd501c6800a97b8
RajakumarSDE <rajakumarsde4@gmail.com>
2026-04-23 08:38:32 +0530

    day 02

---
+++ day02.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class day02 {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+            System.out.println("Hello " + name + " SDE-1");
+        }
+    }
+}
