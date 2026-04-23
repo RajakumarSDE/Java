d8f8b004b6fac41430a102906bc96485b95f640e
RajakumarSDE <rajakumarsde4@gmail.com>
2026-04-23 14:32:16 +0530

    day02

--- day02.java
+++ day02.java
@@ -1,7 +1,18 @@
+
 public class day02 {
+
     public static void main(String[] args) {
-        int mynumber = 10;
-        double mystring = 55.5;
-        System.out.println(mystring * mynumber);
+        long phone = 9751063375L;
+        char mychar = 'A';
+        String mess = "Hello Raja";
+        System.out.println("Let's see something new");
+        System.out.println(phone);
+        System.out.println(mychar);
+        System.out.println(mess.toUpperCase());
+        mymethod();
+    }
+
+    private static void mymethod() {
+        System.out.println("This is a method");
     }
 }
